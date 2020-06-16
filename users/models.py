fa1220017bf4b257d5be3e57755d53d389c5c55b
Do Hwna Kim <ehghkswodnjs@naver.com>
2020-06-16 18:58:30 +0900

    #3.3 Finishing User Model

--- users/models.py
+++ users/models.py
@@ -13,6 +13,32 @@ class User(AbstractUser):
         (GENDER_OTHER, "Other"),
     )
 
-    avatar = models.ImageField(null=True)
-    gender = models.CharField(choices=GEBDER_CHOICES, max_length=10, null=True)
-    bio = models.TextField(default="")
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREA = "kr"
+
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENGLISH, "English"),
+        (LANGUAGE_KOREA, "Korea"),
+    )
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICES = (
+        (CURRENCY_USD, "USD"),
+        (CURRENCY_KRW, "KRW"),
+    )
+
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(
+        choices=GEBDER_CHOICES, max_length=10, null=True, blank=True
+    )
+    bio = models.TextField(default="", blank=True)
+    birthdate = models.DateField(null=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES, max_length=2, null=True, blank=True
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES, max_length=2, null=True, blank=True
+    )
+    superhost = models.BooleanField(default=False)
