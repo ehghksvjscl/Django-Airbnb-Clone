15b5ac129a2269108264168ad24e2a9abbea39d5
Do Hwna Kim <ehghkswodnjs@naver.com>
2020-06-16 18:17:51 +0900

    #3.2 First Model Fields

--- users/models.py
+++ users/models.py
@@ -3,4 +3,16 @@ from django.db import models
 
 
 class User(AbstractUser):
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GEBDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    avatar = models.ImageField(null=True)
+    gender = models.CharField(choices=GEBDER_CHOICES, max_length=10, null=True)
     bio = models.TextField(default="")
