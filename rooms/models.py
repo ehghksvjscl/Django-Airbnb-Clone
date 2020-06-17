b6352855ac45c10ad09a0476f62c10e5433f04ac
Do Hwna Kim <ehghkswodnjs@naver.com>
2020-06-18 05:09:59 +0900

    #4.5 Meta Class and Photo Model

--- rooms/models.py
+++ rooms/models.py
@@ -24,28 +24,46 @@ class RoomType(AbstractItem):
 
     """ Room Type Object Definition """
 
-    pass
+    class Meta:
+        verbose_name_plural = "Room Type"
 
 
 class Amenity(AbstractItem):
 
     """ Amenity Object Definition """
 
-    pass
+    class Meta:
+        verbose_name_plural = "Amenities"
 
 
 class Facility(AbstractItem):
 
     """ Facility Object Definition """
 
-    pass
+    class Meta:
+        verbose_name_plural = "Facilities"
 
 
 class HouseRule(AbstractItem):
 
     """ HouseRule Object Definition """
 
-    pass
+    class Meta:
+        verbose_name = "House Rule"
+
+
+class Photo(core_models.TimeStampedModel):
+
+    """ Photo Objdect Definition """
+
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+
+    # Photo가 위에 있는데도 아래에 있는 Room class를 사용하고 싶다면 문자열로 만들면 된다.
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.caption
 
 
 class Room(core_models.TimeStampedModel):
@@ -67,9 +85,9 @@ class Room(core_models.TimeStampedModel):
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey(users_models.User, on_delete=models.CASCADE)
     room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
-    amenities = models.ManyToManyField(Amenity)
-    facilities = models.ManyToManyField(Facility)
-    house_rules = models.ManyToManyField(HouseRule)
+    amenities = models.ManyToManyField(Amenity, blank=True)
+    facilities = models.ManyToManyField(Facility, blank=True)
+    house_rules = models.ManyToManyField(HouseRule, blank=True)
 
     # Admin에 보여줄 텍스트 정의
     def __str__(self):
