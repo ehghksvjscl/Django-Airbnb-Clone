cc15831fdf3bbe3209b441a0e111d7b7685d7eed
Do Hwna Kim <ehghkswodnjs@naver.com>
2020-06-18 01:04:32 +0900

    #4.3 ManyToMany like a Boss

--- rooms/models.py
+++ rooms/models.py
@@ -4,10 +4,31 @@ from users import models as users_models
 from core import models as core_models
 
 
+class AbstractItem(core_models.TimeStampedModel):
+
+    """ Abstract Item """
+
+    # DB정의
+    name = models.CharField(max_length=80)
+
+    # 추상화
+    class Meta:
+        abstract = True
+
+    # 문자열
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstractItem):
+    pass
+
+
 class Room(core_models.TimeStampedModel):
 
     """ Room Model Definition """
 
+    # DB정의
     name = models.CharField(max_length=140)
     description = models.TextField()
     country = CountryField()
@@ -21,3 +42,8 @@ class Room(core_models.TimeStampedModel):
     check_out = models.TimeField()
     instant_book = models.BooleanField(default=False)
     host = models.ForeignKey(users_models.User, on_delete=models.CASCADE)
+    room_type = models.ManyToManyField(RoomType, blank=True)
+
+    # Admin에 보여줄 텍스트 정의
+    def __str__(self):
+        return self.name
