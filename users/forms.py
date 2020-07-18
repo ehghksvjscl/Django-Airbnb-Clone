40ff202a4b646827e367c2e682fa0377ce7849d3
Do Hwna Kim <ehghkswodnjs@naver.com>
2020-07-18 19:42:24 +0900

    #20.10 Signup

--- users/forms.py
+++ users/forms.py
@@ -3,8 +3,10 @@ from . import models
 
 
 class LoginForm(forms.Form):
-    email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"placeholder": "Email"}))
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={"placeholder": "Password"})
+    )
 
     def clean(self):
         password = self.cleaned_data.get("password")
@@ -25,12 +27,23 @@ class SignUpForm(forms.ModelForm):
     class Meta:
         model = models.User
         fields = ("first_name", "last_name", "email")
+        # 필드로 선언한 폼 수정 작업 방법
+        widgets = {
+            "first_name": forms.TextInput(attrs={"placeholder": "Fist Name"}),
+            "last_name": forms.TextInput(attrs={"placeholder": "Last Nmae"}),
+            "email": forms.TextInput(attrs={"placeholder": "Email"}),
+        }
 
     # first_name = forms.CharField(max_length=80)
     # last_name = forms.CharField(max_length=80)
     # email = forms.EmailField()
-    password = forms.CharField(widget=forms.PasswordInput)
-    password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm Password")
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={"placeholder": "Password"})
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={"placeholder": "Password"}),
+        label="Confirm Password",
+    )
 
     # def clean_email(self):
     #     email = self.cleaned_data.get("email")
--- users/views.py
+++ users/views.py
@@ -57,13 +57,6 @@ class SignUpView(FormView):
     template_name = "users/signup.html"
     form_class = forms.SignUpForm
     success_url = reverse_lazy("core:home")
-    initial = {
-        "first_name": "Kim",
-        "last_name": "Do",
-        "email": "it@las.com",
-        "password": "",
-        "password1": "",
-    }
 
     def form_valid(self, form):
         form.save()
